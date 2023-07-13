cb492caf5ea8ea286be440c987369ebce0441b5b
fggutierrez98 <fggutierrez98@gmail.com>
2023-07-13 16:05:27 -0300

    login google

--- front/crm-williams/src/app/page.tsx
+++ front/crm-williams/src/app/page.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import styles from "./page.module.css";
 import Navbar from "./components/Navbar";
 import Login from "./components/Login";
+import "dotenv/config.js";
 export default function Home() {
   return (
     <>
