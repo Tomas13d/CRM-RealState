67b78c6522249a3f3f5bba33453772ebbcee576c
tomiapo <tomasapochian@gmail.com>
2023-07-13 17:13:57 -0300

    clean unused routes

--- back/api/routes/user.routes.ts
+++ back/api/routes/user.routes.ts
@@ -4,5 +4,5 @@ import UserController from "../controllers/user.controller";
 
 usersRouter.post("/login", UserController.loginUser);
 usersRouter.post("/register", UserController.registerUser);
-usersRouter.post("/register-google", UserController.registerGoogle);
+
 export default usersRouter;
