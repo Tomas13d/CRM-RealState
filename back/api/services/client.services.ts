51d0075ac6d4a3850a4bf0899d8043be7442bff1
tomiapo <tomasapochian@gmail.com>
2023-07-18 16:30:51 -0300

    add createClient service

---
+++ back/api/services/client.services.ts
@@ -0,0 +1,19 @@
+import "firebase/compat/auth";
+import { auth, db } from "../firebase";
+
+type Client = {
+  email: string;
+  first_name: string;
+  last_name: string;
+  is_owner: boolean;
+  is_buyer: boolean;
+  is_tenant: boolean;
+  assigned_agent_name?: string;
+  assigned_agent_id?: string;
+};
+
+export const createClient = async (client: Client) => {
+  const response = await db.collection("Clients").add(client);
+  console.log(response);
+  return response;
+};
