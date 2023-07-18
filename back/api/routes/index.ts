2fe2d96e3a274dda995a229ce9ce96f943904fde
tomiapo <tomasapochian@gmail.com>
2023-07-18 16:30:38 -0300

    add client router to index

--- back/api/routes/index.ts
+++ back/api/routes/index.ts
@@ -1,7 +1,9 @@
 import express from "express";
 const router = express.Router();
 import usersRouter from "./user.routes";
+import clientRouter from "./client.routes";
 
 router.use("/users", usersRouter);
+router.use("/clients", clientRouter);
 
 export default router;
