15a4b3081fb8adc65bdf998a94d1231d13326acc
tomiapo <tomasapochian@gmail.com>
2023-07-20 19:34:26 -0300

    add service

--- back/api/services/acquisition.services.ts
+++ back/api/services/acquisition.services.ts
@@ -6,3 +6,16 @@ export const createAcquisition = async (acquisition: Acquisition) => {
   const response = await db.collection("Acquisitions").add(acquisition);
   return response;
 };
+
+export const getAcquisitions = async () => {
+  const acquisitionsRef = db.collection("Acquisitions");
+  const snapshot = await acquisitionsRef.get();
+  const acquisitions: Acquisition[] = [];
+
+  snapshot.forEach((doc) => {
+    const data = doc.data() as Acquisition;
+    acquisitions.push(data);
+  });
+
+  return acquisitions;
+};
