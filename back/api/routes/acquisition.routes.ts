ef649cc7637343a8cacc707b5650d2073c741446
tomiapo <tomasapochian@gmail.com>
2023-07-20 19:34:15 -0300

    add route

--- back/api/routes/acquisition.routes.ts
+++ back/api/routes/acquisition.routes.ts
@@ -3,5 +3,6 @@ const acquisitionRouter = express.Router();
 import AcquisitionController from "../controllers/acquisition.controller";
 
 acquisitionRouter.post("/create", AcquisitionController.createAcquisition);
+acquisitionRouter.get("/", AcquisitionController.getAllAcquisitions);
 
 export default acquisitionRouter;
