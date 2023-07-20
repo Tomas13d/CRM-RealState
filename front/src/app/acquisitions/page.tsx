86397c05e460be3f4c022739a478338060c12cd5
tomiapo <tomasapochian@gmail.com>
2023-07-20 11:08:28 -0300

    add placeholder page

---
+++ front/src/app/acquisitions/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <div style={{ color: "white" }}>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias veritatis
+      tempore eos nesciunt velit optio. Et, natus repellat dolorum veritatis
+      aliquid facere autem reprehenderit ut nihil cum velit voluptatibus optio!
+    </div>
+  );
+};
+
+export default page;
