cbc59f8ced7e6c199b25bd3c53d690745eb02b82
tomiapo <tomasapochian@gmail.com>
2023-07-20 19:36:11 -0300

    delete file

--- front/src/app/api/acquisitions.ts
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-export const getAllEstates = async () => {
-  const estates = await axios.get("http:localhost:3001/api/estates", {
-    withCredentials: true,
-  });
-
-  // return states
-};
