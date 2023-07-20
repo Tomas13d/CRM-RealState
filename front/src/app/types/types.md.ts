fb3a63ebfd1906f1e88958842018f2f333b138ac
tomiapo <tomasapochian@gmail.com>
2023-07-20 19:33:04 -0300

    create module file

---
+++ front/src/app/types/types.md.ts
@@ -0,0 +1,60 @@
+export type Acquisition = {
+  description: string;
+  agent_id?: string;
+  buyer_id?: string;
+  tenant_id?: string;
+  estate_id: string;
+  transaction_type: string;
+  transaction_price: number;
+  transaction_currency: string;
+  transaction_date: string;
+};
+
+export type Client = {
+  id?: string;
+
+  email: string;
+  first_name: string;
+  last_name: string;
+  isOwner: boolean;
+  isBuyer: boolean;
+  isTenant: boolean;
+  assigned_agent_name?: string;
+  assigned_agent_id?: string;
+};
+
+export type Estate = {
+  id?: string;
+  name: string;
+  address: string;
+  description: string;
+  city: string;
+  expenses_price: number;
+  images?: Array<string>;
+  is_for_rent: boolean;
+  is_for_sale: boolean;
+  owner_id: string;
+  sale_price: number;
+  rent_price: number;
+  currency: string;
+  category: string;
+  rooms: number;
+};
+
+export interface User {
+  firstname: string;
+  lastname: string;
+  type?: string;
+  password: string;
+  email: string;
+  id: string;
+}
+
+export type RawNewAcquisition = {
+  description: string;
+  estateID: string;
+  buyerOrTenantID: string;
+  transactionType: string;
+  transactionCurrency: string;
+  transactionPrice: string;
+};
