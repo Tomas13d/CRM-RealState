de19f917c6b284f291eba41d1644a6e91ab169c5
tomiapo <tomasapochian@gmail.com>
2023-07-25 21:34:11 -0300

    create single estate modal

--- front/src/app/estates/[estateID]/page.tsx
+++ front/src/app/estates/[estateID]/page.tsx
@@ -1,73 +1,20 @@
 "use client";
 import * as React from "react";
+import { Grid, Modal, Button, Box, Divider, Typography } from "@mui/material";
+import CloseButton from "@/app/commons/buttons/closeButton";
+import { H4, H5 } from "@/app/commons/headlines";
 import {
-  Grid,
-  Modal,
-  Typography,
-  Button,
-  Box,
-  Divider,
-  IconButton,
-} from "@mui/material";
-import CloseIcon from "@mui/icons-material/Close";
-
-const style = {
-  position: "absolute" as "absolute",
-  top: "50%",
-  left: "50%",
-  transform: "translate(-50%, -50%)",
-  width: 800,
-  bgcolor: "#263348",
-  border: "2px solid #000",
-  boxShadow: 24,
-  p: 4,
-  color: "white",
-};
+  Subtitle1,
+  SubtitleDesciption1,
+} from "@/app/commons/subtitles/index.tsx";
+import PrimaryButton from "@/app/commons/buttons/primaryButton";
+import ModalBox from "@/app/commons/ModalBox";
 import { Estate } from "@/app/types/types.md";
 
 export default function SingleEstateModal(estate: Estate) {
-  //replace placeholder data with Estate info
   const [open, setOpen] = React.useState(false);
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
-  const h4Style = { fontSize: "24px", fontWeight: "bold" };
-  const h5Style = { fontSize: "24px" };
-  const buttonStyle = {
-    color: "white",
-    "&:hover": {
-      backgroundColor: "white",
-      color: "#6778d6",
-    },
-    bgcolor: "#6778d6",
-    fontWeight: "bold",
-    fontSize: "14px",
-    borderRadius: "25px",
-    padding: "10px 20px",
-    textTransform: "none",
-  };
-  const closeButtonStyle = {
-    color: "white",
-    "&:hover": {
-      backgroundColor: "white",
-      color: "#6778d6",
-    },
-    bgcolor: "#6778d6",
-    fontSize: "40px",
-    borderRadius: "10px",
-  };
-
-  const subtitleStyle = {
-    color: "white",
-    fontSize: "14px",
-    fontWeight: "bold",
-    marginBottom: "5px",
-  };
-  const subtitleBodyStyle = {
-    color: "white",
-    fontSize: "14px",
-
-    marginBottom: "15px",
-  };
 
   return (
     <Box>
@@ -78,12 +25,10 @@ export default function SingleEstateModal(estate: Estate) {
         aria-labelledby="modal-modal-title"
         aria-describedby="modal-modal-description"
       >
-        <Box sx={style}>
+        <ModalBox>
           <Grid container spacing={3}>
             <Grid item xs={1}>
-              <IconButton onClick={handleClose}>
-                <CloseIcon sx={closeButtonStyle} />
-              </IconButton>
+              <CloseButton onClick={handleClose} />
             </Grid>
             <Grid
               item
@@ -92,30 +37,26 @@ export default function SingleEstateModal(estate: Estate) {
               flexDirection={"column"}
               justifyContent={"center"}
             >
-              <Typography sx={{ ...h4Style, marginBottom: "10px" }}>
-                Titulo del Departamento
-              </Typography>
+              <H4 bold={true}>Título del Departamento</H4>
             </Grid>
             <Grid item xs={3}>
-              <Button sx={buttonStyle}>Iniciar chat</Button>
+              <PrimaryButton>Iniciar chat</PrimaryButton>
             </Grid>
           </Grid>
-
-          <Typography sx={{ ...h5Style, margin: "10px 0px" }}>
-            Informacion general
+          <H5>
+            Información general
             <Divider
               sx={{ width: "95%", bgcolor: "#576B7E", margin: "10px 0px" }}
             ></Divider>
-          </Typography>
-
+          </H5>
           <Grid container spacing={2} sx={{ margin: "10px 0px" }}>
             <Grid item xs={6}>
-              <Typography sx={subtitleStyle}>Propietario</Typography>
-              <Typography sx={subtitleBodyStyle}>Juan Perez</Typography>
-              <Typography sx={subtitleStyle}>Teléfono de contacto</Typography>
-              <Typography sx={subtitleBodyStyle}>11 1234 5678</Typography>
-              <Typography sx={subtitleStyle}>Email</Typography>
-              <Typography sx={subtitleBodyStyle}>ejemplo@email.com</Typography>
+              <Subtitle1>Propietario</Subtitle1>
+              <SubtitleDesciption1>Juan Perez</SubtitleDesciption1>
+              <Subtitle1>Teléfono de contacto</Subtitle1>
+              <SubtitleDesciption1>11 1234 5678</SubtitleDesciption1>
+              <Subtitle1>Email</Subtitle1>
+              <SubtitleDesciption1>ejemplo@email.com</SubtitleDesciption1>
             </Grid>
             <Grid item xs={6} display={"flex"} justifyContent={"center"}>
               <Box
@@ -133,18 +74,16 @@ export default function SingleEstateModal(estate: Estate) {
           </Grid>
           <Grid container spacing={2} sx={{ margin: "10px 0px" }}>
             <Grid item xs={6}>
-              <Typography sx={subtitleStyle}>Tipo de producto</Typography>
-              <Typography sx={subtitleBodyStyle}>Casa</Typography>
-              <Typography sx={subtitleStyle}>Dirección</Typography>
-              <Typography sx={subtitleBodyStyle}>
-                Direccion falsa 123
-              </Typography>
+              <Subtitle1>Tipo de producto</Subtitle1>
+              <SubtitleDesciption1>Casa</SubtitleDesciption1>
+              <Subtitle1>Dirección</Subtitle1>
+              <SubtitleDesciption1>Direccion falsa 123</SubtitleDesciption1>
             </Grid>
             <Grid item xs={6}>
-              <Typography sx={subtitleStyle}>Tipo de operación</Typography>
-              <Typography sx={subtitleBodyStyle}>Venta</Typography>
-              <Typography sx={subtitleStyle}>Barrio</Typography>
-              <Typography sx={subtitleBodyStyle}>Los Alamos</Typography>
+              <Subtitle1>Tipo de operación</Subtitle1>
+              <SubtitleDesciption1>Venta</SubtitleDesciption1>
+              <Subtitle1>Barrio</Subtitle1>
+              <SubtitleDesciption1>Los Alamos</SubtitleDesciption1>
             </Grid>
             <Divider
               sx={{ width: "95%", bgcolor: "#576B7E", margin: "20px 0px" }}
@@ -152,11 +91,11 @@ export default function SingleEstateModal(estate: Estate) {
           </Grid>
           <Grid container spacing={1} sx={{ margin: "20px 0px" }}>
             <Grid item xs={6}>
-              <Typography sx={subtitleStyle}>Agente asignado</Typography>
-              <Typography sx={subtitleBodyStyle}>Don Draper</Typography>
+              <Subtitle1>Agente asignado</Subtitle1>
+              <SubtitleDesciption1>Don Draper</SubtitleDesciption1>
             </Grid>
           </Grid>
-        </Box>
+        </ModalBox>
       </Modal>
     </Box>
   );
