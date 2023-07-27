1de71f0b359c61fd25e7efcd14abf84d58871ee8
tomiapo <tomasapochian@gmail.com>
2023-07-27 15:42:25 -0300

    delete file

--- back/api/routes/agent.routes.ts
+++
@@ -1,7 +0,0 @@
-import express from "express";
-import AgentController from "../controllers/agent.controller";
-const agentRouter = express.Router();
-
-agentRouter.get("/", AgentController.getAllAgents);
-
-export default agentRouter;
