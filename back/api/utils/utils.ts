1072d68f2e12e84453ead7bdb39ace7ce2389f0e
LautaroGuinez <laucha_09@live.com>
2023-07-27 21:59:49 -0300

    new methods to clear undefined objetcs

--- back/api/utils/utils.ts
+++ back/api/utils/utils.ts
@@ -7,3 +7,9 @@ export const isValidPassword = (password: string) => {
   const passwordRegex = /^(?=.*[A-Z])(?=.*[^a-zA-Z0-9]).{6,}$/;
   return passwordRegex.test(password);
 };
+
+export const removeUndefined = (obj: { [key: string]: any }) => {
+  return Object.fromEntries(
+    Object.entries(obj).filter(([_, v]) => v !== undefined)
+  );
+};
