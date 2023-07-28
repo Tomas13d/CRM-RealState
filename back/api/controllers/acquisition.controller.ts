2b409e4c708f3c6d1945eb3ab3e40831c86cc164
LautaroGuinez <laucha_09@live.com>
2023-07-27 21:59:12 -0300

    adding new way to create acquisition and saleroute

--- back/api/controllers/acquisition.controller.ts
+++ back/api/controllers/acquisition.controller.ts
@@ -2,6 +2,7 @@ import { Request, Response } from "express";
 import {
   createAcquisition,
   getAcquisitions,
+  getAllAcquisitionsSales,
 } from "../services/acquisition.services";
 
 class AcquisitionController {
@@ -11,6 +12,7 @@ class AcquisitionController {
       const acquisitionID = response.path.split("/")[1];
       return res.status(201).send({ ...req.body, id: acquisitionID });
     } catch (error) {
+      console.log(error);
       return res.status(400).json({ msg: "Error creating acquisition", error });
     }
   }
@@ -18,6 +20,15 @@ class AcquisitionController {
   static async getAllAcquisitions(_req: Request, res: Response) {
     try {
       const acquisitions = await getAcquisitions();
+      res.status(200).send(acquisitions);
+    } catch (error) {
+      res.status(400).json({ msg: "Error retrieving Acquisitions", error });
+    }
+  }
+
+  static async getAllAcquisitionsSales(_req: Request, res: Response) {
+    try {
+      const acquisitions = await getAllAcquisitionsSales();
       res.status(200).send(acquisitions);
     } catch (error) {
       res.status(400).json({ msg: "Error retrieving Acquisitions", error });
