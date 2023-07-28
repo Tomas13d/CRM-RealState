9fe32e76bf5645d4205b0d30b81c8282779d4408
LautaroGuinez <laucha_09@live.com>
2023-07-27 21:59:26 -0300

    new types creates

--- back/api/services/types.md.ts
+++ back/api/services/types.md.ts
@@ -1,12 +1,25 @@
 type SupportedCurrencies = "ARS" | "USD";
 
-export type Acquisition = {
+export type AcquisitionData = {
   description: string;
   agent_id: string;
   owner_id: string;
-  buyer_id: string;
-  tenant_id: string;
+  buyer_id?: string;
+  tenant_id?: string;
   estate_id: string;
+  transaction_type: string;
+  transaction_price: number;
+  transaction_currency: string;
+  transaction_date: string;
+};
+
+export type Acquisition = {
+  description: string;
+  agent: User;
+  owner: Client;
+  buyer?: Client;
+  tenant?: Client;
+  estate: Estate;
   transaction_type: string;
   transaction_price: number;
   transaction_currency: string;
@@ -25,7 +38,7 @@ export type Client = {
   assigned_agent_id?: string;
 };
 
-export type Estate = {
+export type estateData = {
   id?: string;
   name: string;
   address: string;
@@ -34,6 +47,24 @@ export type Estate = {
   expenses_price?: number;
   images?: Array<string>;
   owner_id: string;
+  sale_price?: number;
+  rent_price?: number;
+  rent_currency?: SupportedCurrencies;
+  sale_currency?: SupportedCurrencies;
+  category: "house" | "garage" | "complex" | "flat" | "shop" | "office";
+  rooms: number;
+  operation_type: "rent" | "sale" | "rent_sale";
+};
+
+export type Estate = {
+  id?: string;
+  name: string;
+  address: string;
+  description: string;
+  city: string;
+  expenses_price?: number;
+  images?: Array<string>;
+  owner: Client;
   sale_price?: number;
   rent_price?: number;
   rent_currency?: SupportedCurrencies;
