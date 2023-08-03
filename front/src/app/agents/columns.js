aa9db826939b87f6a5f0ea1bd665445b54653ee2
tomiapo <tomasapochian@gmail.com>
2023-08-03 16:47:02 -0300

    add columns

---
+++ front/src/app/agents/columns.js
@@ -0,0 +1,8 @@
+const columns = [
+  { key: "firstname", label: "Nombre" },
+  { key: "lastname", label: "Apellido" },
+  { key: "acquisition_number", label: "N° Captaciones" },
+  { key: "button", label: "" },
+];
+
+export default columns;
