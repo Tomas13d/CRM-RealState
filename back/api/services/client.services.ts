80c4f7e704e141b021c73f7a148f2a66e7268542
tomiapo <tomasapochian@gmail.com>
2023-08-03 20:32:34 -0300

    add logic for client type

--- back/api/services/client.services.ts
+++ back/api/services/client.services.ts
@@ -3,6 +3,17 @@ import { db } from "../firebase";
 import { Client } from "./types.md";
 
 export const createClient = async (client: Client) => {
+  let type: string;
+  if (client.is_buyer) {
+    type = "Comprador";
+  } else if (client.is_owner) {
+    type = "Propietario";
+  } else if (client.is_tenant) {
+    type = "Inquilino";
+  } else {
+    type = "";
+  }
+  client.type = type;
   const response = await db.collection("Clients").add(client);
   return response;
 };
