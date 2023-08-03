b08674bdc09712c96b37cc11b4c8695af6e2cb5b
belmontst <sebastian_buscaglia@outlook.com>
2023-08-03 04:03:39 -0300

    ruta de logout

--- back/dist/server.js
+++ back/dist/server.js
@@ -6,12 +6,14 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
+const cookieParser = require("cookie-parser");
 require("dotenv").config();
 const routes_1 = __importDefault(require("./api/routes"));
 const PORT = process.env.port || 3001;
 const app = express();
 app.use(morgan("dev"));
 app.use(express.json());
+app.use(cookieParser());
 app.use(cors({
     origin: process.env.CORS_ORIGIN || "http://localhost:3000",
     credentials: true,
